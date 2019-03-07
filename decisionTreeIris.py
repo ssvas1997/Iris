ae08cdce62c6d874849710cb23589f8ca463878b
ssvas1997 <37727994+ssvas1997@users.noreply.github.com>
2019-03-07 10:34:16 +0530

    Add files via upload

--- decisionTreeIris.py
+++ decisionTreeIris.py
@@ -36,3 +36,24 @@ petw= input("Petal width")
 print(sepl,sepw,petl,petw)
 pr=dectree.predict(np.column_stack([sepl,sepw,petl,petw]))
 print("Predicted Species is:",pr)
+
+
+import os
+#os.environ["PATH"]= 'C:/Program Fles(x86)/Graphvz2.38/bin'
+os.environ["PATH"]= 'E:\\graphviz-2.38\\release\\bin'
+
+from sklearn.externals.six import StringIO
+from IPython.display import Image
+from sklearn.tree import export_graphviz
+import pydotplus
+
+dot_data= StringIO()
+feat_col=['Sepal length','Sepal width','Petal length','Petal width']
+export_graphviz(dectree, out_file=dot_data, filled=True, rounded=True, 
+                special_characters=True, feature_names=feat_col,
+                class_names=['Setosa','Versicolor','Virginica'])
+graph= pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('iris.png')
+Image(graph.create_png())
+
+
